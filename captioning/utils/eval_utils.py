70d7d9fc6d4ca6b2b8c90bb61548fd07ed26e9d7
Joshua Lo <33582835+joshrclo@users.noreply.github.com>
2021-05-04 16:23:09 -0400

    Update eval_utils.py

--- captioning/utils/eval_utils.py
+++ captioning/utils/eval_utils.py
@@ -168,12 +168,13 @@ def eval_split(model, crit, loader, eval_kwargs={}):
         with torch.no_grad():
             tmp_eval_kwargs = eval_kwargs.copy()
             tmp_eval_kwargs.update({'sample_n': 1})
-            outputs, out_encoder = model(fc_feats, att_feats, att_masks, opt=tmp_eval_kwargs, mode='sample')
+            seq, seq_logprobs = model(fc_feats, att_feats, att_masks, opt=tmp_eval_kwargs, mode='sample')
             
-            print(outputs.shape, out_encoder.shape)
-
-            seq, seq_logprobs = outputs
+            out_encoder = model.encode(att_feats, att_masks)
+            
             seq = seq.data
+            print(seq.shape, seq_logprobs.shape, out_encoder.shape)
+            
             entropy = - (F.softmax(seq_logprobs, dim=2) * seq_logprobs).sum(2).sum(1) / ((seq>0).to(seq_logprobs).sum(1)+1)
             perplexity = - seq_logprobs.gather(2, seq.unsqueeze(2)).squeeze(2).sum(1) / ((seq>0).to(seq_logprobs).sum(1)+1)
         
